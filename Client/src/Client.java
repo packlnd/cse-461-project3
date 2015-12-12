a1e477a17a0580e24d52bd3ae3c19f48c24a5fa1
Patrik Ackland <patrikackland@gmail.com>
2015-12-11 20:44:04 -0800

    Done stuff

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,9 +1,13 @@
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
@@ -23,47 +27,67 @@ public class Client {
 		}
 	}
 
-    private void fakeClient() {
-        
-    }
-    
-    private void fakeServer() {
+	private void fakeClient() {
+		System.out.println("fakeClient");
+		byte[] buf = new byte[1000];
+		DatagramPacket dp = new DatagramPacket(buf, buf.length);
+		try {
+			while (true) {
+				DatagramSocket s = new DatagramSocket();
+				s.receive(dp);
+				System.out.println("Here");
+				BufferedImage img = ImageIO.read(new ByteArrayInputStream(dp.getData()));
+				JFrame window = new JFrame("fakeClient");
+				window.getContentPane().add(new JLabel(new ImageIcon(img)));
+				window.setResizable(true);
+				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				window.pack();
+				window.setVisible(true);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+	}
 
+	private void fakeServer() {
+		System.out.println("fakeServer");
 		Webcam webcam = Webcam.getDefault();
 		webcam.setViewSize(WebcamResolution.VGA.getSize());
 
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-		panel.setDisplayDebugInfo(true);
-		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
+		try {
+			DatagramSocket s = new DatagramSocket();
+			InetAddress hostAddress = InetAddress.getByName("localhost");
+			while (true) {
+				BufferedImage img = webcam.getImage();
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				byte[] bytes = baos.toByteArray();
+				DatagramPacket out = new DatagramPacket(bytes, bytes.length, hostAddress, 9999);
+				s.send(out);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("done");
+	}
 
-		JFrame window = new JFrame("Test webcam panel");
-		window.add(panel);
-		window.setResizable(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.pack();
-		window.setVisible(true);
-    }
+	private boolean isMaster(String s) {
+		return !s.equals("$");
+	}
 
-    private boolean isMaster(String s) {
-        return !s.equals("$");
-    }
-
-    private String communicateWithServer() {
-        final String host = "localhost";
-        final int port = 1234;
-        String response = null;
-        try {
-            Socket serverSocket = new Socket(host, port);
-            BufferedReader fromServer =
-                new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-            response = fromServer.readLine();
-            serverSocket.close();
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        }
-        return response;
-    }
+	private String communicateWithServer() {
+		final String host = "localhost";
+		final int port = 1234;
+		String response = null;
+		try {
+			Socket serverSocket = new Socket(host, port);
+			BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			response = fromServer.readLine();
+			serverSocket.close();
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+		return response;
+	}
 
 }
