8a2432a9038655a57022a320bb2857a5d0b6fc64
Patrik Ackland <patrikackland@gmail.com>
2015-12-13 00:15:18 -0800

    Now more proper GUI, cant launch two clients atm

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,9 +1,12 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,75 +16,122 @@ import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
 
 public class Client {
+
+	private Webcam webcam;
+	private JLabel remoteCam;
+
 	public static void main(String[] args) {
 		new Client();
 	}
 
 	public Client() {
-		String response = communicateWithServer();
-		try {
-			//TODO: Client's should act the same. No need for fake server. Problem is localhost with only one camera.
-			// ALso get address to peer from response.
-			if (isMaster(response)) {
-				fakeServer();
-			} else {
-				fakeClient();
+		initializeWebcam();
+		buildGUI();
+		sendFrames();
+		getFrames();
+	}
+
+	private void sendFrames() {
+		Thread t = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						BufferedImage img = webcam.getImage();
+						ByteArrayOutputStream baos = new ByteArrayOutputStream();
+						ImageIO.write(img, "jpg", baos);
+						byte[] bytes = baos.toByteArray();
+						DatagramSocket ds;
+						InetAddress ip = InetAddress.getByName("localhost");
+						ds = new DatagramSocket();
+						System.out.println(bytes.length);
+						DatagramPacket dp = new DatagramPacket(bytes, bytes.length, ip, 3000);
+						ds.send(dp);
+						ds.close();
+					} catch (Exception e) {
+					}
+				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		};
+		t.start();
 	}
 
-	private void fakeClient() throws Exception {
-		DatagramSocket ds = new DatagramSocket(3000);
-		
-		//TODO: 25000 is large. Don't think we want 25k bytes per packet.
-		byte[] buf = new byte[25000];
-		DatagramPacket dp = new DatagramPacket(buf, 25000);
-		
-		//TODO: Loop receive
-		ds.receive(dp);
-		InputStream in = new ByteArrayInputStream(dp.getData());
-		BufferedImage img = ImageIO.read(in);
-		JFrame frame = new JFrame();
-		frame.getContentPane().add(new JLabel(new ImageIcon(img)));
-		frame.pack();
-		frame.setVisible(true);
-		ds.close();
+	private void getFrames() {
+		Thread t = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						DatagramSocket ds = new DatagramSocket(3000);
+						byte[] buf = new byte[25000];
+						DatagramPacket dp = new DatagramPacket(buf, 25000);
+						ds.receive(dp);
+						InputStream in = new ByteArrayInputStream(dp.getData());
+						BufferedImage img = ImageIO.read(in);
+						JFrame frame = new JFrame();
+						frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+					} catch (Exception e) {
+					}
+				}
+			}
+		};
+		t.run();
 	}
 
-	private void fakeServer() throws Exception {
-
-		Webcam webcam = Webcam.getDefault();
+	private void initializeWebcam() {
+		webcam = Webcam.getDefault();
 		webcam.setViewSize(WebcamResolution.VGA.getSize());
+	}
 
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-		panel.setDisplayDebugInfo(true);
-		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
+	private void buildGUI() {
+		// TODO: Is webcamera already in use? Two clients on same host
+		WebcamPanel panel = new WebcamPanel(webcam, true);
+		panel.setSize(new Dimension(250, 500));
+		remoteCam = new JLabel(new ImageIcon(webcam.getImage()));
+		remoteCam.setPreferredSize(new Dimension(250, 500));
+		remoteCam.setMinimumSize(new Dimension(250, 500));
+		remoteCam.setMaximumSize(new Dimension(250, 500));
+		remoteCam.setBackground(Color.BLACK);
 
 		JFrame window = new JFrame("Test webcam panel");
+		window.setPreferredSize(new Dimension(500, 500));
 		window.add(panel);
+		window.add(remoteCam);
 		window.setResizable(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
 		window.setVisible(true);
-
-		//TODO: Loop, compress
-		BufferedImage img = webcam.getImage();
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write(img, "jpg", baos);
-		byte[] bytes = baos.toByteArray();
-		DatagramSocket ds;
-		InetAddress ip = InetAddress.getByName("localhost");
-		ds = new DatagramSocket();
-		System.out.println(bytes.length);
-		DatagramPacket dp = new DatagramPacket(bytes, bytes.length, ip, 3000);
-		ds.send(dp);
-		ds.close();
 	}
 
+	/*
+	 * private void fakeClient() throws Exception { DatagramSocket ds = new
+	 * DatagramSocket(3000);
+	 * 
+	 * //TODO: 25000 is large. Don't think we want 25k bytes per packet. byte[]
+	 * buf = new byte[25000]; DatagramPacket dp = new DatagramPacket(buf,
+	 * 25000);
+	 * 
+	 * //TODO: Loop receive ds.receive(dp); InputStream in = new
+	 * ByteArrayInputStream(dp.getData()); BufferedImage img = ImageIO.read(in);
+	 * JFrame frame = new JFrame(); frame.getContentPane().add(new JLabel(new
+	 * ImageIcon(img))); frame.pack(); frame.setVisible(true); ds.close(); }
+	 * 
+	 * private void fakeServer() throws Exception { WebcamPanel panel = new
+	 * WebcamPanel(webcam); panel.setFPSDisplayed(true);
+	 * panel.setDisplayDebugInfo(true); panel.setImageSizeDisplayed(true);
+	 * panel.setMirrored(true);
+	 * 
+	 * JFrame window = new JFrame("Test webcam panel"); window.add(panel);
+	 * window.setResizable(true);
+	 * window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); window.pack();
+	 * window.setVisible(true);
+	 * 
+	 * //TODO: Loop, compress BufferedImage img = webcam.getImage();
+	 * ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	 * ImageIO.write(img, "jpg", baos); byte[] bytes = baos.toByteArray();
+	 * DatagramSocket ds; InetAddress ip = InetAddress.getByName("localhost");
+	 * ds = new DatagramSocket(); System.out.println(bytes.length);
+	 * DatagramPacket dp = new DatagramPacket(bytes, bytes.length, ip, 3000);
+	 * ds.send(dp); ds.close(); }
+	 */
 	private boolean isMaster(String s) {
 		return !s.equals("$");
 	}
